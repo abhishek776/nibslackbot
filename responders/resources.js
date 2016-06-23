e440448966f0b4b2fa9d8c08c844f6fcd27b7571
Abhishek Fatehpuria <abhishekfatehpuria776@gmail.com>
2016-06-23 01:34:17 -0700

    new commit

--- responders/resources.js
+++ responders/resources.js
@@ -1,10 +1,10 @@
 var resources = function(payload,callback){
   console.log(payload)
   if (payload.channel_name === 'project_managets') {
-    this.respond("Drive: https://drive.google.com/open?id=0B_oM8kBUTj-BRFFkRVlFczZPa3M"
+    callback(null, "Drive: https://drive.google.com/open?id=0B_oM8kBUTj-BRFFkRVlFczZPa3M"
                   + "Client Database: https://airtable.com/shrNPTSCLWcnJ94Gv")
   } else {
-    this.respond("No resources for this channel.")
+    callback(null, "No resources for this channel.")
   }
 }
 

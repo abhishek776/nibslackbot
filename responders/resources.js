933157246e93b1a7d915fdf75a9d9925c5271928
Abhishek Fatehpuria <abhishekfatehpuria776@gmail.com>
2016-06-23 01:10:28 -0700

    resources

---
+++ responders/resources.js
@@ -0,0 +1,12 @@
+var resources = function(payload,callback){
+  if (payload.channel_name === 'project_managets') {
+    return callback(null, `Drive: https://drive.google.com/open?id=0B_oM8kBUTj-BRFFkRVlFczZPa3M
+                          Client Database: https://airtable.com/shrNPTSCLWcnJ94Gv`)
+  } else {
+    return callback(null, 'No resources for this channel.')
+  }
+
+
+resources.command = 'resources'
+
+module.exports = resources
